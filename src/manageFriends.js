9146266e8364b0b19d26f9d1ded3a7980b893a66
Mazin <code.mazin.m@gmail.com>
2025-05-22 12:58:16 +1000

    tested reducers

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,38 @@
-export function manageFriends(state, action) {
+const initialState = { 
+  friends: []
+};
+
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        ...state,
+        friends: [...state.friends, action.payload]
+      };
+    case "friends/remove":
+      return { 
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.payload)
+      };
+    default:
+      return state;
+  }
 }
+
+// let friend = { id: 100, name: "Avi", hometown: "NYC" };
+// const result = initialState.filter((friend.id = id))
+
+const addFriendAction = {
+  type: 'friends/add',
+  payload: {
+    name: 'Chrome Boi',
+    hometown: 'NYC',
+    id: 1
+  }
+};
+
+const removeFriendAction = {
+  type: 'friends/remove',
+  payload: 1
+};
